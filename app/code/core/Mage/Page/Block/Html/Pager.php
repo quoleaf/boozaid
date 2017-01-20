8b7a3cb0a54af8320a477a86fa01042db4358d9b
rejlayson <rej@quoleaf.com>
2017-01-20 14:43:27 +0800

    updated files + blog plugin

--- app/code/core/Mage/Page/Block/Html/Pager.php
+++ app/code/core/Mage/Page/Block/Html/Pager.php
@@ -38,7 +38,7 @@ class Mage_Page_Block_Html_Pager extends Mage_Core_Block_Template
     protected $_collection = null;
     protected $_pageVarName    = 'p';
     protected $_limitVarName   = 'limit';
-    protected $_availableLimit = array(10=>10,20=>20,50=>50);
+    protected $_availableLimit = array(6=>6,20=>20,50=>50);
     protected $_dispersion     = 3;
     protected $_displayPages   = 5;
     protected $_showPerPage    = true;
